a381e859fa333dc8dc0ea0efe57d071784563fa0
RenanGroh <renangroh@gmail.com>
2024-02-07 20:05:55 -0300

    classes ator e diretor criadas

---
+++ src/Ator.java
@@ -0,0 +1,27 @@
+public class Ator {
+
+    private String nome;
+    private String identidade;
+
+    public Ator(String nome, String identidade) {
+        this.nome = nome;
+        this.identidade = identidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdentidade() {
+        return identidade;
+    }
+
+    public void setIdentidade(String identidade) {
+        this.identidade = identidade;
+    }
+}
+
