2f0448c63c529453f974b8ebaab658ea887efaca
Gabrielly <gabriellybsoares23@gmail.com>
2024-02-23 18:17:10 -0300

    Atualizando classes Pessoa e Diretor

--- src/models/Diretor.java
+++ src/models/Diretor.java
@@ -1,2 +1,37 @@
-package models;public class Diretor {
+package models;
+
+import util.Enums;
+
+public class Diretor extends Pessoa {
+
+    private Enums.AreaDiretor area;
+
+    private String cpf;
+
+    public Enums.AreaDiretor getArea() {
+        return area;
+    }
+
+    private String getCpf() {
+        return cpf;
+    }
+
+    public void setArea(Enums.AreaDiretor area) {
+        this.area = area;
+    }
+
+    public Diretor(String nome, int idade, Enums.AreaDiretor area) {
+        super(nome, idade);
+        this.cpf = util.GeradorCPF.gerarCPF();
+        this.area = area;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + this.getNome() + "\n Idade: " + this.getIdade() + "\n Identidade: " + this.getCpf() + "\n Área: " + this.getArea();
+    }
+
+
 }
--- src/models/Pessoa.java
+++ src/models/Pessoa.java
@@ -1,2 +1,49 @@
-package models;public class Pessoa {
+package models;
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private String identidade;
+
+    public Pessoa(String nome, int idade) {
+        validaString(nome, "Nome");
+        validaIdade(idade);
+
+
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // GETTERS E SETTERS
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        validaString(nome, "Nome");
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        validaIdade(idade);
+        this.idade = idade;
+    }
+
+
+
+    private static void validaString(String valor, String nomeAtributo) {
+        if (valor == null || valor.isEmpty()) {
+            throw new IllegalArgumentException(nomeAtributo + " inválido!");
+        }
+    }
+
+    private void validaIdade(int idade) {
+        if (idade < 0) {
+            throw new IllegalArgumentException("Idade inválida!");
+        }
+    }
 }
